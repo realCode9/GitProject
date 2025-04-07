eeb0eb393f8c609ed666daebdf85f6dd6c5584e7
Vijay Bhosle <vijay.bhosle@emids.com>
2025-04-07 15:58:27 +0530

    Merge remote branch to local infor

---
+++ CopyAnotherBranchCode.py
@@ -0,0 +1,7 @@
+#   add local changes first -> git add .
+#   then stash changes so that they will disappear from the terminal -> git stash
+#   then see what all branches are there at remote --> git fetch --all
+#   take pull from the desired branch --> git pull origin branch_name
+#   apply git stash so that stashed changes will come back --> git stash apply
+#   now see are there any conflicts are and resolve them
+#   see the status now after resolving conflicts -> git status
