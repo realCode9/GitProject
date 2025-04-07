8ff7dd6a498f07afb985f385e151062af870a458
Vijay Bhosle <vijay.bhosle@emids.com>
2025-04-07 15:47:51 +0530

    Merge happened - stash - fetch apply - pull - stash apply - resolve - git status - commit

--- LocalCode.py
+++ LocalCode.py
@@ -1,1 +1,3 @@
 print("Local code need to sync with OldBranchCode.py")
+
+#this is code to be shown in new branch taht has to be conflicated with the code 
--- PrintHello.py
+++ PrintHello.py
@@ -1,3 +1,4 @@
 print("Hello World")
+# Need the code from the local branch
 
 #this line code is conflicting with the new branch loacal code
